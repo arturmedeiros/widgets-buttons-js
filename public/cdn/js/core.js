180b5bf21ba86e74c3a7c37c301e5934e2b99de1
Artur <arjos.eu@outlook.com>
2022-09-07 17:27:49 -0300

    URL Adjustment CDN

--- public/cdn/js/core.js
+++ public/cdn/js/core.js
@@ -9,7 +9,7 @@ export let open = false
 // export const url = '.'
 // export const url = 'http://localhost:8080/cdn'
 // export const url = 'http://192.168.15.5:8080/cdn'
-export const url = 'https://raw.githubusercontent.com/arturmedeiros/widgets-buttons-js/master/public/cdn'
+export const url = 'https://widgets-buttons.pages.dev/cdn'
 
 /* Options */
 
